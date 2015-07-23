a721613ea842266d5b083179289be553e7466d1d
Alistair MacDonald <al@pwn.io>
2015-07-23 11:20:04 -0400

    Adding tests to cover comfigs in readme.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,130 +25,70 @@ module.exports = function(grunt) {
 
     // Before generating any new files, remove any previously-created files.
     clean: {
-      tests: ['tmp']
+      tests: ['dest']
     },
 
     cdn_switch: {
-      // EXAMPLE from README:
-      // 'myDevTarget': {
-      //   files: {
-      //     // Left: file to write to, right: file to consume
-      //     'dest/index.html': ['templates/index.html']
-      //   },
-      //   options: {
-      //     // When false, this switch causes cdn_swith to render the HTML
-      //     // block with links to local files
-      //     cdn: false,
-      //     // Always fetch resources when remote files are newer that local
-      //     // or when local files do not exist.
-      //     fetch_new: true,
 
-      //     // "blocks" refers to HTML comment blocks that we want to replace
-      //     blocks: {
-      //     // "Blocks" means comment blocks, which denotes an HTML comment,
-      //     // Eg: <!-- Hello World! -->. In our case, we want to replace the
-      //     // comment <!--cdn-switch:javascript--> with an HTML block of
-      //     // resources that might be available on CDN, Eg: a block of
-      //     // <script> tags for JavaScript.
-
-      //       // Lets define our first comment-block replacement...
-      //       javascript: {
-
-      //         // "local_path" points to the local directory you would like
-      //         // your CDN resources saved to when fetched
-      //         local_path: 'dest/js',
-
-      //         // "html" refers to the html that you want to wrap around the
-      //         // resource URL.
-      //         html: '<script src="{{resource}}"></script>',
-
-      //         // "resources" is a list of remote resources that you want to
-      //         // inject where the HTML comment block used to be.
-      //         resources: [
-      //           'http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min.js',
-      //           'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.js',
-      //           'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.13/angular-ui-router.min.js',
-      //         ],
-      //       },
-
-      //       // Here we define our second block, this time, for CSS resources,
-      //       // we are still defining this block in the same HTML file as before.
-      //       css: {
-
-      //         // "local-path" is where ee will store the local versions of these files
-      //         local_path: 'dest/css',
-
-      //         // We want to wrap our CSS resources in <LINK> tags.
-      //         html: '<link href="{{resource}}" rel="stylesheet"/>',
-
-      //         // And we only have once CSS resource to load
-      //         resources: [
-      //           'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
-      //         ],
-      //       },
-      //     },
-      //   },
-      // },
-      'myDevTarget': {
+      'basic-local-config': {
         files: {
-          'tmp/index-dev.html': ['test/fixtures/index.html']
+          'dest/basic/basic-local-config.html': ['test/fixtures/basic-local-config.html']
         },
         options: {
+          use_local: true,
           blocks: {
             javascript: {
-              local_path: 'tmp/static',
+              local_path: 'dest/basic/js',
               html: '<script src="{{resource}}"></script>',
               resources: [
                 'http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min.js',
                 'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.js',
                 'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.13/angular-ui-router.min.js',
-                'http://tombatossals.github.io/angular-leaflet-directive/dist/angular-leaflet-directive.min.js',
-                'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
-                'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js',
-                'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js',
-                'http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js',
-                'http://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js',
               ],
             },
-            css: {
-              local_path: 'tmp/static',
-              html: '<link href="{{resource}}" rel="stylesheet"/>',
-              resources: [
-                'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
-              ],
-            },
-          },
-        },
+          }
+        }
       },
-      'myProdTarget': {
+
+      'newer-local-config': {
         files: {
-          'tmp/index-prod.html': ['test/fixtures/index.html']
+          'dest/newer/newer-local-config.html': ['test/fixtures/newer-local-config.html']
         },
         options: {
+          use_local: {
+            fetch_newer: true,
+          },
           blocks: {
             javascript: {
+              local_path: 'dest/newer/js',
               html: '<script src="{{resource}}"></script>',
               resources: [
-                'http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.15/angular.min.js',
-                'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.js',
-                'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.13/angular-ui-router.min.js',
-                'http://tombatossals.github.io/angular-leaflet-directive/dist/angular-leaflet-directive.min.js',
-                'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js',
-                'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js',
-                'http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js',
-                'http://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js',
-                'http://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js',
+                'http://code.jquery.com/jquery-latest.js',
               ],
             },
-            css: {
-              html: '<link href="{{resource}}" rel="stylesheet"/>',
-              resources: [
-                'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css',
-              ],
-            },
-          },
-        },
+          }
+        }
       },
+
+
+      'cdn-config': {
+        files: {
+          'dest/cdn/cdn-config.html': ['test/fixtures/cdn-config.html']
+        },
+        options: {
+          use_local: false,
+          blocks: {
+            javascript: {
+              local_path: 'dest/cdn/js',
+              html: '<script src="{{resource}}"></script>',
+              resources: [
+                'http://code.jquery.com/jquery-latest.js',
+              ],
+            },
+          }
+        }
+      },
+
     },
 
     // Unit tests.
@@ -172,5 +112,5 @@ module.exports = function(grunt) {
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
-  grunt.registerTask('default', ['cdn_switch:myDevTarget']);
+  // grunt.registerTask('default', ['cdn_switch']);
 };
--- test/cdn_switch_test.js
+++ test/cdn_switch_test.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var grunt = require('grunt');
+var grunt = require('grunt')
+  , fs = require('fs')
+  , Promise = require('bluebird')
+  ;
+
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,28 +26,117 @@ var grunt = require('grunt');
     test.ifError(value)
 */
 
+
+var exists = function (file) {
+  return new Promise(function (resolve, reject) {
+    fs.exists(file, function (file_exists) {
+      if (file_exists) {
+        resolve(file);
+      } else {
+        reject(file);
+      }
+    });
+  });
+};
+
+
+
 exports.cdn_switch = {
   setUp: function(done) {
     // setup here if necessary
     done();
   },
-  dev_options: function(test) {
-    test.expect(1);
 
-    var actual = grunt.file.read('tmp/index-dev.html');
-    var expected = grunt.file.read('test/expected/index-dev.html');
-    test.equal(actual, expected, 'default dev settings should work.');
 
-    test.done();
+  basic_local_config: function(test) {
+    test.expect(2);
+
+    var actual = grunt.file.read('dest/basic/basic-local-config.html');
+    var expected = grunt.file.read('test/expected/basic-local-config.html');
+    test.equal(actual, expected, 'basic-local-config should work.');
+
+
+    var promiseStack = [
+      exists('dest/basic/js/angular-animate.js'),
+      exists('dest/basic/js/angular-ui-router.min.js'),
+      exists('dest/basic/js/angular.min.js'),
+    ];
+
+    Promise.settle(promiseStack).then(function(results){
+      var errorCount = 0;
+
+      // Log errors when things are not fetched...
+      results.forEach(function (result) {
+        if (!result.isFulfilled()) {
+          errorCount += 1;
+          console.log('LOCAL FILE DOES NOT EXIST:');
+          console.log(result);
+        }
+      });
+
+      test.equal(errorCount, 0);
+      test.done();
+    });
   },
-  prod_options: function(test) {
-    test.expect(1);
 
-    var actual = grunt.file.read('tmp/index-prod.html');
-    var expected = grunt.file.read('test/expected/index-prod.html');
-    test.equal(actual, expected, 'default prod settings should work.');
+  newer_local_config: function(test) {
+    test.expect(2);
 
-    test.done();
+    var actual = grunt.file.read('dest/newer/newer-local-config.html');
+    var expected = grunt.file.read('test/expected/newer-local-config.html');
+    test.equal(actual, expected, 'newer-local-config should work.');
+
+
+    var promiseStack = [
+      exists('dest/newer/js/jquery-latest.js'),
+    ];
+
+    Promise.settle(promiseStack).then(function(results){
+      var errorCount = 0;
+
+      // Log errors when things are not fetched...
+      results.forEach(function (result) {
+        if (!result.isFulfilled()) {
+          errorCount += 1;
+          console.log('LOCAL FILE DOES NOT EXIST:');
+          console.log(result);
+        }
+      });
+
+      test.equal(errorCount, 0);
+      test.done();
+    });
+  },
+
+
+  cdn_config: function(test) {
+    test.expect(2);
+
+    var actual = grunt.file.read('dest/cdn/cdn-config.html');
+    var expected = grunt.file.read('test/expected/cdn-config.html');
+    test.equal(actual, expected, 'cdn-config should work.');
+
+
+    var promiseStack = [
+      exists('dest/newer/js/jquery-latest.js'),
+    ];
+
+    Promise.settle(promiseStack).then(function(results){
+      var errorCount = 0;
+
+      // Log errors when things are not fetched...
+      results.forEach(function (result) {
+        if (!result.isFulfilled()) {
+        } else {
+          errorCount += 1;
+          console.log('LOCAL SHOULD NOT NOT EXIST (but:');
+          console.log(result);
+        }
+      });
+
+      test.equal(errorCount, 1);
+      test.done();
+    });
   },
 
 };
